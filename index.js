79e2014a9a974720720b843eaef8625b60e66aa6
Akshay-Tarde <akshaymtarde@gmail.com>
2023-02-10 15:17:20 +0530

    Styled the countdown numbers

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const hours = document.querySelector('.hours');
 const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
 
-const starting_days = 1;
-let total_time = starting_days * 24 * 60 * 60;
+const starting_days = 5;
+let total_time = starting_days * 24 * 60 * 60 + 10;
 // let total_time = 86420;
 setInterval(countdown, 1000);
 
 function countdown() {
-    const days_remaining = Math.floor(total_time / (60*60*24) );
-    const hours_remaining = Math.floor(total_time / ( 60 * 60) ) % 24 ;
-    const minutes_remaining = Math.floor( (total_time / 60) % 60) ;
+    let days_remaining = Math.floor(total_time / (60*60*24) );
+    let hours_remaining = Math.floor(total_time / ( 60 * 60) ) % 24 ;
+    let minutes_remaining = Math.floor( (total_time / 60) % 60) ;
     let seconds_remaining = total_time % 60;
 
     seconds_remaining = seconds_remaining < 10 ? "0" + seconds_remaining : seconds_remaining;
+    minutes_remaining = minutes_remaining < 10 ? "0" + minutes_remaining : minutes_remaining;
+    hours_remaining = hours_remaining < 10 ? "0" + hours_remaining : hours_remaining;
+    days_remaining = days_remaining < 10 ? "0" + days_remaining : days_remaining;
     days.innerHTML = `${days_remaining}`;
     hours.innerHTML = `${hours_remaining}`;
     minutes.innerHTML = `${minutes_remaining}`;
